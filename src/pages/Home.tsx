ec7bb5f5a52efe326f86453c87425db2cc8882db
cho4u4o <cho4u4o@gmail.com>
2024-10-08 00:56:14 +0900

    delete : delete unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled'
-import Header from '../components/Header'
 import rice from '../assets/rice.svg'
 import kiryong from '../assets/kiryong-notice.svg'
 import MenuCardWrapper from '../components/MenuCardWrapper'
