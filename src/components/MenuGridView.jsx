6694b8d7f5bcdcc61090815bbed4af9ad160e5ff
cho4u4o <cho4u4o@gmail.com>
2024-11-15 22:51:27 +0900

    feat : integrate dummy data with menu grid view

--- src/components/MenuGridView.jsx
+++ src/components/MenuGridView.jsx
@@ -1,22 +1,48 @@
-import styled from '@emotion/styled'
-import menuData from '../datas/menus.json'
+import styled from '@emotion/styled';
+import menuData from '../datas/menus.json';
+
+export default function MenuGridView({ id }) {
+  const menus = menuData.Menus;
+  return (
+    <MenuContainer>
+      {menus[id] ? (
+        <MenuGrid>
+          {menus[id]['쌀국수'].map((menu, index) => (
+            <MenuItem key={index}>
+              <MenuImage url={menu.url} />
+              <MenuTitle>{menu.menu}</MenuTitle>
+              <MenuTitle>{menu.price.toLocaleString()}원</MenuTitle>
+            </MenuItem>
+          ))}
+        </MenuGrid>
+      ) : (
+        <NoMenu>
+          <p>메뉴가 없습니다.</p>
+        </NoMenu>
+      )}
+    </MenuContainer>
+  );
+}
 
 const MenuContainer = styled.div`
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+  overflow-y: scroll;
+`;
 
 const MenuGrid = styled.main`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 20px;
   width: 100%;
-`
+`;
 
 const MenuItem = styled.div`
   background-color: transparent;
-  border-radius: 20px;
+  border-radius: 15px;
   padding: 10px;
   transition:
     transform 0.3s,
@@ -26,7 +52,7 @@ const MenuItem = styled.div`
   &:hover {
     transform: scale(1.05);
   }
-`
+`;
 
 const MenuImage = styled.div`
   background-image: url(${(props) => props.url});
@@ -34,21 +60,19 @@ const MenuImage = styled.div`
   margin-bottom: 10px;
   height: 80%;
   border-radius: 10px;
-`
+`;
 
-const menus = menuData.Menus
+const MenuTitle = styled.p`
+  font: 600 18px/1.5 Pretendard;
+  margin: 0;
+  color: white;
+`;
 
-export default function MenuGridView() {
-  return (
-    <MenuContainer>
-      <MenuGrid>
-        {menus.map((menu, index) => (
-          <MenuItem key={index}>
-            <MenuImage url={menu.url} />
-            {menu.menu}
-          </MenuItem>
-        ))}
-      </MenuGrid>
-    </MenuContainer>
-  )
-}
+const NoMenu = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  color: white;
+  place-items: center;
+  font: 600 20px/1.5em Pretendard;
+`;
