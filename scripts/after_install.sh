b31f76d6760968471b8f921363c93bd6c79db092
cho4u4o <cho4u4o@gmail.com>
2024-11-17 20:24:16 +0900

    delete : delete files - became unused changing deployment settings

--- scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "AfterInstall 단계 실행 중"
-
-# 빌드 파일 복사
-sudo cp -r * /var/www/html/
-
-# 소유권 및 권한 설정
-sudo chown -R nginx:nginx /var/www/html
-sudo chmod -R 755 /var/www/html
--- scripts/start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "ApplicationStart 단계 실행 중"
-
-# Nginx 시작 또는 재시작
-sudo systemctl enable nginx
-sudo systemctl restart nginx
