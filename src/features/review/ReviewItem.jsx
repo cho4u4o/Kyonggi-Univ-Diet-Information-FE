8a5a54cebe96a230f666c5e41a805c67f4166413
cho4u4o <cho4u4o@gmail.com>
2024-12-06 13:36:34 +0900

    fix : fix 3

--- src/features/review/ReviewItem.jsx
+++ src/features/review/ReviewItem.jsx
@@ -10,9 +10,7 @@ export default function ReviewItem({ review }) {
 
   async function fetchFavCnt() {
     const cnt = await axios.get(requests.fetchReviewFav + review.id);
-    if (cnt > 0) {
-      setFavCount(cnt);
-    }
+    setFavCount(cnt);
   }
 
   async function fetchIsFaved() {
@@ -29,6 +27,7 @@ export default function ReviewItem({ review }) {
       } else {
         setFav(false);
       }
+      console.log(fav);
     } catch (error) {}
   }
 
